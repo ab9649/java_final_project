bc703d68143603b1e2be86e72812db0493b786cd
Ames Bartels <105522725+ab9649@users.noreply.github.com>
2022-12-01 09:23:29 -0500

    Update Bishop.java

--- pieces/Bishop.java
+++ pieces/Bishop.java
@@ -19,47 +19,51 @@ public class Bishop extends Piece{
         int y = super.getY();
         Set<String> retList = new HashSet<String>();
         
-        checkX = x-1;
-        checkY = y;
+        int checkX = x-1;
+        int checkY = y-1;
       //check NW
         while(checkX >=0 && checkY >= 0 && gameArray[checkX][checkY].getPiece() == null){
           retList.add(checkX + "," + checkY);
           checkX --;
           checkY --;
         }
-      if (checkX >=0 && checkY >= 0 && !gameArray[checkX][checkY].getPiece().getColor().equals(super.getColor())){
+        if (checkX >=0 && checkY >= 0 && !gameArray[checkX][checkY].getPiece().getColor().equals(super.getColor())){
         retList.add(checkX + "," + checkY);
-      }
+        }
       //check NE
-      while(checkX <=7 && checkY >= 0 && gameArray[checkX][checkY].getPiece() == null){
+        checkX = x+1;
+        checkY = y-1;
+        while(checkX <=7 && checkY >= 0 && gameArray[checkX][checkY].getPiece() == null){
           retList.add(checkX + "," + checkY);
           checkX ++;
           checkY --;
         }
-      if (checkX <=7 && checkY >= 0 && !gameArray[checkX][checkY].getPiece().getColor().equals(super.getColor())){
+        if (checkX <=7 && checkY >= 0 && !gameArray[checkX][checkY].getPiece().getColor().equals(super.getColor())){
         retList.add(checkX + "," + checkY);
-      }
+        }
       //SW
-      while(checkY <= 7 && checkX >= 0 && gameArray[checkX][checkY].getPiece() == null){
+        checkX = x-1;
+        checkY = y+1;
+        while(checkY <= 7 && checkX >= 0 && gameArray[checkX][checkY].getPiece() == null){
           retList.add(checkX + "," + checkY);
           checkX --;
           checkY ++;
         }
-      if (checkY >= 0 && !gameArray[checkX][checkY].getPiece().getColor().equals(super.getColor())){
+        if (checkY >= 0 && !gameArray[checkX][checkY].getPiece().getColor().equals(super.getColor())){
         retList.add(checkX + "," + checkY);
-      }
+        }
       //SE
-      while(check X <= 7 && checkY <= 7 && gameArray[checkX][checkY].getPiece() == null){
+        checkX = x+1;
+        checkY = y+1;
+        while(check X <= 7 && checkY <= 7 && gameArray[checkX][checkY].getPiece() == null){
           retList.add(checkX + "," + checkY);
           checkX ++:
           checkY ++;
         }
-      if (checkX <= 7 && checkY <= 7 && !gameArray[checkX][checkY].getPiece().getColor().equals(super.getColor())){
+        if (checkX <= 7 && checkY <= 7 && !gameArray[checkX][checkY].getPiece().getColor().equals(super.getColor())){
         retList.add(checkX + "," + checkY);
-      }
+        }
       
         return retList;
-    }
-
-    
+    }   
 }
